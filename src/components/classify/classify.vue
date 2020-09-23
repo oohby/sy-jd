3e93178d3d4cd3fe7dac0f06d15ecd1523d0012f
oohby <oohby_000@126.com>
2020-09-23 20:19:18 +0800

    first commit

---
+++ src/components/classify/classify.vue
@@ -0,0 +1,369 @@
+<template>
+  <div class="sort">
+    <div class="sort-head">
+      <p class="sort-head-p" @click="gosearch"><span>畅想20</span></p>
+    </div>
+    <div class="sort-content">
+      <div class="sort-left">
+          <div class="sort-left-div" v-for="(item,index) in list" :key="index"><a href="#" @click="tab(index)" :class="{active:num==index}">{{item}}</a></div>
+      </div>
+      <div class="sort-right">
+          <div class="sort-right-content" v-for="(item,index) in contentlist" :key="index" v-show="index==num">
+              <div class="sort-right-content-headimg">
+                <img :src="item.headimg" alt="">
+              </div>
+              <div class="sort-right-content-title">{{item.name}}</div>
+              <div class="sort-right-content-box">
+                  <div class="sort-right-content-img" v-for="(item,index) in item.contentimg" :key="index" @click="gocart(index)">
+                      <img :src="item.img" alt="">
+                      <p style="fontSize:13px;color:#666">{{item.title}}</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+      return{
+        num:0,
+        list:["新品","华为手机","荣耀手机","笔记本","平板","智能穿戴&VR","智能屏","智能家居","耳机音箱","专属配件","通用配件","生态产品","增值服务","智能计算"],
+        contentlist:[
+        {
+            "headimg":require("../../assets/c11.jpg"),
+            "name":"手机",
+            "contentimg":[
+                {
+                  "img":require("../../assets/c11.jpg"),
+                  "title":"HUAWEI P40 5G"
+                },
+                {
+                  "img":require("../../assets/c11.jpg"),
+                  "title":"HUAWEI P40 Pro+"
+                },
+                {
+                  "img":require("../../assets/c11.jpg"),
+                  "title":"HUAWEI P40 5G"
+                },
+                {
+                  "img":require("../../assets/c11.jpg"),
+                  "title":"HUAWEI P40 Pro+"
+                }
+            ]
+        },
+        {
+            "headimg":require("../../assets/c21.jpg"),
+            "name":"HUAWEI P系列",
+            "contentimg":[
+                {
+                  "img":require("../../assets/c21.jpg"),
+                  "title":"HUAWEI P40 5G"
+                },
+                {
+                  "img":require("../../assets/c21.jpg"),
+                  "title":"HUAWEI P40 Pro+"
+                },
+                {
+                  "img":require("../../assets/c21.jpg"),
+                  "title":"HUAWEI P40 5G"
+                },
+                {
+                  "img":require("../../assets/c21.jpg"),
+                  "title":"HUAWEI P40 Pro+"
+                }
+            ]
+        },
+        {
+            "headimg":require("../../assets/c23.jpg"),
+            "name":"荣耀 V系列",
+            "contentimg":[
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 5G"
+                },
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 Pro+"
+                }
+            ]
+        },
+        {
+            "headimg":require("../../assets/c23.jpg"),
+            "name":"笔记本电脑",
+            "contentimg":[
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 5G"
+                },
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 Pro+"
+                }
+            ]
+        },
+        {
+            "headimg":require("../../assets/c23.jpg"),
+            "name":"手机",
+            "contentimg":[
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 5G"
+                },
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 Pro+"
+                }
+            ]
+        },
+        {
+            "headimg":require("../../assets/c23.jpg"),
+            "name":"手机",
+            "contentimg":[
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 5G"
+                },
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 Pro+"
+                }
+            ]
+        },
+        {
+            "headimg":require("../../assets/c23.jpg"),
+            "name":"手机",
+            "contentimg":[
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 5G"
+                },
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 Pro+"
+                }
+            ]
+        },
+        {
+            "headimg":require("../../assets/c23.jpg"),
+            "name":"手机",
+            "contentimg":[
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 5G"
+                },
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 Pro+"
+                }
+            ]
+        },
+        {
+            "headimg":require("../../assets/c23.jpg"),
+            "name":"手机",
+            "contentimg":[
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 5G"
+                },
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 Pro+"
+                }
+            ]
+        },
+        {
+            "headimg":require("../../assets/c23.jpg"),
+            "name":"手机",
+            "contentimg":[
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 5G"
+                },
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 Pro+"
+                }
+            ]
+        },
+        {
+            "headimg":require("../../assets/c23.jpg"),
+            "name":"手机",
+            "contentimg":[
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 5G"
+                },
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 Pro+"
+                }
+            ]
+        },
+        {
+            "headimg":require("../../assets/c23.jpg"),
+            "name":"手机",
+            "contentimg":[
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 5G"
+                },
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 Pro+"
+                }
+            ]
+        },
+        {
+            "headimg":require("../../assets/c23.jpg"),
+            "name":"手机",
+            "contentimg":[
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 5G"
+                },
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 Pro+"
+                }
+            ]
+        },
+        {
+            "headimg":require("../../assets/c23.jpg"),
+            "name":"手机",
+            "contentimg":[
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 5G"
+                },
+                {
+                  "img":require("../../assets/c23.jpg"),
+                  "title":"HUAWEI P40 Pro+"
+                }
+            ]
+        }
+    ]
+      }
+    },
+
+    methods:{
+      tab(index){
+        console.log(index);
+        this.num = index
+      },
+      gocart(index){
+        console.log(index);
+      },
+      gosearch(){
+        this.$router.push('/search')
+      }
+    }
+};
+</script>
+
+<style scoped>
+.sort {
+  width: 100%;
+  height: 100%;
+}
+
+.sort-head {
+  width: 100%;
+  height: 8%;
+   /* padding-top: 5px; */
+   text-align: center;
+}
+
+.sort-head-p {
+  margin-left: 20px;
+  width: 80%;
+  height: 70%;
+  background: #f5f5f5;
+  border-radius: 30px;
+  position: relative;
+  top: 3px;
+}
+
+.sort-head-p span{
+    color: rgb(194, 190, 190);
+  font-size: 14px;
+  line-height: 33px;
+  margin-left: 10px;
+}
+
+.sort-content {
+  width: 100%;
+  height: 92%;
+  display: flex;
+  overflow: auto;
+}
+
+.sort-left{
+    width: 30%;
+    height: 100%;
+    overflow: auto;
+}
+
+.sort-left-div{
+    width: 100%;
+    height: 51px;
+}
+
+.sort-left-div a{
+    display: inline-block;
+    width: 80%;
+    margin-left: 15%;
+    font-size: 15px;
+    color: #000;
+    padding-top: 5px;
+}
+
+.active{
+  border-right: 3px solid #e01d20;
+}
+
+.sort-right{
+    width: 70%;
+    height: 100%;   
+    overflow: auto;
+}
+
+.sort-right-content{
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+}
+
+.sort-right-content-headimg,.sort-right-content-headimg>img{
+  width: 100%;
+  height: 80px;
+}
+
+.sort-right-content-title{
+  width: 100%;
+  height: 46px;
+  text-align: center;
+  line-height: 46px;
+}
+
+.sort-right-content-box{
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.sort-right-content-img{
+  width: 33.3%;
+  height: 124px;
+  text-align: center;
+}
+
+.sort-right-content-img>img{
+  width: 55px;
+  height: 55px;
+  margin-top: 20px;
+}
+</style>
---
+++ src/components/my/signUp/signUp.vue
@@ -0,0 +1,95 @@
+<template>
+  <!-- 注册页面 -->
+  <div>
+    <!-- 顶部 -->
+    <div class="header">
+      <i
+        class="iconfont icon-back"
+        style="color:#909295;font-size:25px;margin:0 10px;"
+        @click="back"
+      ></i>
+      <span class="headerText">京东注册</span>
+      <div class="block"></div>
+    </div>
+
+    <!-- 注册 -->
+    <div class="signUp">
+      <input v-model="userName" type="text" placeholder="请输入用户名">
+      <input v-model="telNum" type="text" placeholder="请输入手机号">
+      <input v-model="password" type="password" placeholder="请输入密码">
+      <input v-model="passwordAgain" type="password" placeholder="请再次输入密码">
+      <div class="btn">
+        <button class="signUpBtn" @click="signUp">注册</button>
+        <button class="loginBtn" @click="login">已有账号，去登录</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import { MessageBox } from "mint-ui";
+
+export default {
+  data() {
+    return {
+      userName: "",
+      telNum: "",
+      password: "",
+      passwordAgain: ""
+    };
+  },
+  methods: {
+    back() {
+      this.$router.go(-1);
+    },
+
+    // 判断用户输入的信息是否符合几个简单的条件，若符合则传递数据到后端查询数据库
+    signUp() {
+      if (
+        this.userName === "" ||
+        this.telNum === "" ||
+        this.password === "" ||
+        this.passwordAgain === ""
+      ) {
+        MessageBox("提示", "请将注册信息填写完整！");
+      } else {
+        if (this.userName.length < 4) {
+          MessageBox("提示", "用户名不得少于4位！");
+        } else {
+          if (this.telNum.length !== 11) {
+            MessageBox("提示", "请输入正确的手机号！");
+          } else {
+            if (this.password !== this.passwordAgain) {
+              MessageBox("提示", "两次输入的密码不一致！");
+            } else {
+              axios.post("http://jd.hhp.im/signUp", {
+                  userName: this.userName,
+                  telNum: this.telNum,
+                  password: this.password
+                }).then(result => {
+                  var result = result.data;
+                  if (result === "注册成功！") {
+                    MessageBox("提示", result);
+                    this.$router.push({ path: "/login" });
+                  } else {
+                    MessageBox("提示", result);
+                  }
+                });
+            }
+          }
+        }
+      }
+    },
+
+    // 去登录
+    login() {
+      this.$router.push({ path: "/login" });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./signUp.scss";
+</style>
